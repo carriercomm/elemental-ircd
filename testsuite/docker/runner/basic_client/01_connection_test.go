da662dd081ed8cffdabb42baacf5b80b07c8c4f1
Sam Dodrill <xena@yolo-swag.com>
2015-02-22 02:11:09 -0800

    make basic test more robust

--- testsuite/docker/runner/basic_client/01_connection_test.go
+++ testsuite/docker/runner/basic_client/01_connection_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"sync"
 	"testing"
 
 	"github.com/thoj/go-ircevent"
@@ -19,8 +20,23 @@ func TestMain(m *testing.M) {
 }
 
 func TestBasicConnection(t *testing.T) {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	conn.AddCallback("ERROR", func(e *irc.Event) {
+		t.Fatalf("%s", e.Message())
+	})
+
+	conn.AddCallback("001", func(e *irc.Event) {
+		t.Log(e.Message())
+		wg.Done()
+	})
+
 	err := conn.Connect(os.Getenv("IRCD_PORT_6667_TCP_ADDR") + ":6667")
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	go conn.Loop()
+	wg.Wait()
 }
